516e5a098cc9c1c7a90355b431500ad6339f1efa
patrick <patrick@flyskyler.com>
2023-08-26 01:56:10 -0300

    add buttonComponent

---
+++ src/components/ButtonComponent/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View } from 'react-native';
+import {Feather as Icon} from '@expo/vector-icons'; 
+import { Button } from "@rneui/themed";
+import buttonStyle from './styles';
+
+const ButtonComponent = (props) => {
+    return (
+        <View style={buttonStyle.buttonContainer}>
+            <Button 
+                onPress={props.onPress}
+                buttonStyle={{
+                    backgroundColor: '#E27396',
+                    borderWidth: 0,
+                    borderRadius: 12,
+                    width: 327,
+                    height: 56,
+                    shadowOffset : { width: 0, height: 4},
+                    shadowOpacity: 0.25,
+                    shadowRadius: 4,
+                    elevation: 1,
+                    blurRadius: 4,
+                    textTransform:'uppercase'
+                }}
+            >
+                {props.left &&(<Icon name="arrow-left" color="white" />)}
+                {props.title}
+                {props.right &&(<Icon name="arrow-right" color="white" />)}
+            </Button>
+    </View>
+    );
+  };
+export default ButtonComponent;
---
+++ src/components/ButtonComponent/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const buttonStyle = StyleSheet.create({
+  buttonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical:10
+  },
+});
+
+export default buttonStyle;
