6b961b06256c17e2c276ff22ea39829ac5e256ea
patrick <patrick@flyskyler.com>
2023-08-25 22:38:21 -0300

    some indentations into app.js

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-
 import Routes from './src/routes'
 
 export default function App() {
   
-
   return (
-
-    
     <Routes />
   );
 }
