af4bd74bf2e25a5563d19e21c30e3e4607e14e1a
patrick <patrick@flyskyler.com>
2023-10-15 04:15:43 -0300

    add functions to use the token

---
+++ src/utils/auth.js
@@ -0,0 +1,36 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import api from '../../services/api';
+import {AUTHENTICATION} from '@env';
+
+export async function fetchToken() {
+  try {
+    let tokenOptions = { headers: { "authentication": AUTHENTICATION }}
+    const response = await api.get('/getToken?t=4dm1n', tokenOptions);
+    const token = response.data.token;
+
+    await AsyncStorage.setItem('authToken', token);
+
+    return token;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export async function getToken() {
+  try {
+    const token = await AsyncStorage.getItem('authToken');
+    return token;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export async function removeToken(){
+  try {
+    await AsyncStorage.removeItem('authToken')
+  } catch (error) {
+    console.log(error);
+  }
+}
