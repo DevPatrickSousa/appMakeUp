85f6fda5533bc4d0ab1f56b610dd4e7a3d67dc3c
patrick <patrick@flyskyler.com>
2023-10-15 04:19:04 -0300

    updating profile page and fix some indentation

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -6,47 +6,65 @@ import ButtonComponent from "../../components/ButtonComponent/index"
 import Line from '../../components/Line';
 import InputComponent from '../../components/InputComponent/index';
 import { useFocusEffect } from "@react-navigation/native";
+import {AUTHENTICATION, CONTENT_TYPE} from '@env';
+import api from '../../../services/api';
 
-export default function Profile(){
+export default function Profile({ isAuthenticated, navigation }) {
   const [step, setStep] = useState(0);
   const [data, setData] = useState([]);
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('')
+  const [token, setToken] = useState('');
+
   const { register, handleSubmit, control } = useForm({
     defaultValues: {
       step,
     },
   });
 
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      if (!isAuthenticated) {
+        navigation.navigate('Login');
+      }
+    };
+    checkAuthentication();
+  }, [isAuthenticated, navigation]);
+
+
   const formSteps = [
     {
       title: "Informações pessoais",
       content: (
-        <View style={profile.action}>
-          <InputComponent label="Nome" placeholder="Digite o nome do contato" />
-          <InputComponent label="Telefone" placeholder="Digite o telefone" />
-          <ButtonComponent onRightIconPress={nextStep} right={true} title="contato 1"/>
+        <View style={profile.actionCard}>
+          <InputComponent minHeight='55px' placeholder="Digite o nome do contato" value={name} onChangeText={(text) => setName(text)}/>
+          <InputComponent minHeight='55px' placeholder="Digite o telefone" value={number} onChangeText={(text) => setNumber(text)}/>
+          <ButtonComponent minHeight='40px' minWidth='168px' backgroundColor='#e989ff' borderColor='#ffbbca' onRightIconPress={nextStep} right={true} title="contato 1"/>
         </View>
       ),
     },
     {
       title: "Endereço",
       content: (
-        <View style={profile.action}>
-          <InputComponent label="Nome" placeholder="Digite o nome do contato" />
-          <InputComponent label="Telefone" placeholder="Digite o telefone" />
+        <View style={profile.actionCard}>
+          <InputComponent minHeight='55px' placeholder="Digite o nome do contato" value={name} onChangeText={(text) => setName(text)}/>
+          <InputComponent minHeight='55px' placeholder="Digite o telefone" value={number} onChangeText={(text) => setNumber(text)}/>
           <ButtonComponent onRightIconPress={nextStep} onLeftIconPress={previousStep} left={true} right={true} title="contato 2"/>
-          
-          
         </View>
       ),
     },
     {
       title: "Pagamento",
       content: (
-        <View style={profile.action}>
-          <InputComponent label="Nome" placeholder="Digite o nome do contato" />
-          <InputComponent label="Telefone" placeholder="Digite o telefone" />
-          <ButtonComponent onLeftIconPress={previousStep} left={true} title="contato 3"/>
+        <View style={profile.actionCard}>
+          <InputComponent minHeight='55px' placeholder="Digite o nome do contato" value={name} onChangeText={(text) => setName(text)}/>
+          <InputComponent minHeight='55px' placeholder="Digite o telefone" value={number} onChangeText={(text) => setNumber(text)}/>
           
+          
+          <View style={profile.buttonContainer}>
+          <ButtonComponent minHeight='40px' minWidthContainer='150px' onLeftIconPress={previousStep} left={true} title="contato 3"/>
+          <ButtonComponent minHeight='40px' minWidthContainer='150px' onPress={saveContacts} title="salvar" />
+          </View>
         </View>
       ),
     },
@@ -61,16 +79,29 @@ export default function Profile(){
     setStep(step - 1)
   }
   function nextStep(){
-    const contato = {"nome": "Dybala", "telefone": "(11)2222-2222"}
+    const contato = {"nome": name, "telefone": number}
     const updatedData = [
       ...data,
       contato
     ]
     setData(updatedData);
     console.log(updatedData);
+    setName('');
+    setNumber('');
     setStep(step + 1);
   }
-  //fazer com que toda vez que a tela de profile for aberta os steps e o objeto dada sejam resetados.
+  async function saveContacts(){
+    let option = { headers: { 'Content-Type': CONTENT_TYPE, 'authorization': 'Bearer ' + token } }
+    await api.get('/contacts?key=4dm1n', option)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  //fazer com que toda vez que a tela de profile for aberta os steps e o objeto data sejam resetados.
   useFocusEffect(
     React.useCallback(() => {
       setStep(0);
@@ -78,19 +109,21 @@ export default function Profile(){
     }, [])
   );
 
-  return (
-    
-      <View style={profile.container}>
-        <View style={profile.avatarContainer}>
-              <Image source={require('../../../assets/testeIcon.png')}
-                style={profile.logo}
-                resizeMode="contain"
-              />
+    return (
+        <View style={profile.container}>
+          <View style={profile.avatarContainer}>
+                <Image source={require('../../../assets/logoMaybePurple.png')}
+                  style={profile.logo}
+                  resizeMode="contain"
+                />
+                <View style={profile.appMakeUp}>
+                  <Text style={profile.textAppMakeUp}>
+                    AppMakeUp
+                  </Text>
+                </View>
+          </View>
+          {formSteps[step].content}
         </View>
-        <Line/>
-        {formSteps[step].content}
-
-      </View>
-   
-  );
+    
+    );
 };
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -2,10 +2,10 @@ import { StyleSheet } from 'react-native';
 
 export const profile = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#EA9AB2',
-        alignItems: 'center',
-        justifyContent: 'center',
+      flex: 1,
+      backgroundColor: '#e989ff',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
       },
       stepContent: {
         padding: 20,
@@ -13,7 +13,9 @@ export const profile = StyleSheet.create({
       avatarContainer: {
         alignItems: 'center',
         justifyContent: 'center',
-        paddingVertical:20
+        width: 250,
+        height: 250,
+        marginBottom:15
       },
       logo: {
         width: 194,
@@ -27,7 +29,41 @@ export const profile = StyleSheet.create({
         shadowRadius: 3.45,
         elevation: 1,
       },
-      action:{
-        paddingTop:30
+      actionCard:{
+        height:'auto',
+        width:'auto',
+        bottom:0,
+        backgroundColor: '#e989ff',
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor:'#ffbbca',
+        elevation:6,
+        maxHeight:'320px',
+        minWidth:'100%',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 2,
+          height: 9,
+        },
+        shadowOpacity: 0.09,
+        shadowRadius: 3.45,
+        flex:1
+      },
+      buttonContainer: {
+        flexDirection:'row',
+        gap:10,
+      },
+      textAppMakeUp:{
+        color: '#FFF',
+        fontFamily: 'Roboto',
+        fontSize:40,
+        textShadowColor: '#8025b7', 
+        textShadowOffset: { width: 2, height: 2 }, 
+        textShadowRadius: 5,
+      },
+      saveButton:{
+        marginVertical:10
       }
 });
