74f51c283f1782950622fd3dc23b7ce914a5d9e1
devPatrickSousa <patrickseven22@hotmail.com>
2023-10-20 00:02:20 -0300

    changing token function name

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../../services/api';
 import {AUTHENTICATION} from '@env';
 
-export async function fetchToken() {
+export async function setToken() {
   try {
     let tokenOptions = { headers: { "authentication": AUTHENTICATION }}
     const response = await api.get('/getToken?t=4dm1n', tokenOptions);
