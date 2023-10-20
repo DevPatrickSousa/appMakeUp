c9f0ec8fa081640bb8c1c2540adb002ba9e4b5ed
devPatrickSousa <patrickseven22@hotmail.com>
2023-10-19 23:59:19 -0300

    fix buttonComponent

--- src/components/ButtonComponent/index.jsx
+++ src/components/ButtonComponent/index.jsx
@@ -1,51 +1,60 @@
 import React from 'react';
 import { TouchableOpacity, View, Text } from 'react-native';
-import { Feather as Icon } from '@expo/vector-icons'; 
+import { Feather as Icon } from '@expo/vector-icons';
 import { Button } from "@rneui/themed";
 import buttonStyle from './styles';
 
 const ButtonComponent = (props) => {
-    return (
-        <Button
-              title={props.title}
-              color={props.color}
-              onPress={props.onPress}
-              titleStyle={buttonStyle.buttonText}
-              containerStyle={{
-                width: props.width,
-                minWidth: props.minWidth,
-                maxWidth: props.maxWidth,
-                minHeight: props.minHeight,
-                maxHeight: props.maxHeight,
-                shadowColor: "#000",
-                shadowOffset: {
-                    width: 0,
-                    height: 2,
-                },
-                shadowOpacity: 0.6,
-                shadowRadius: 3,
-                elevation: 3,
-                blurRadius: 4,
-                borderRadius:props.borderRadius,
-                borderBottomStartRadius:props.borderBottomStartRadius,
-                borderBottomEndRadius:props.borderBottomEndRadius,
-                alignSelf:'center'
-              }}
-
-              buttonStyle={{
-        
-              }}
-          
-              icon={{
-                name: props.iconName,
-                type: 'font-awesome',
-                size: 15,
-                color: 'white',
-              }}
-              iconRight
-              iconContainerStyle={{ marginLeft: 10, marginRight: -10 }}
-            />
-    );
+  return (
+    <Button
+      title={props.title}
+      color={props.color}
+      onPress={props.onPress}
+      titleStyle={buttonStyle.buttonText}
+      containerStyle={{
+        width: props.width,
+        height: props.height,
+        minWidth: props.minWidth,
+        maxWidth: props.maxWidth,
+        minHeight: props.minHeightContainer,
+        maxHeight: props.maxHeight,
+        borderRadius: props.borderRadius,
+        borderBottomStartRadius: props.borderBottomStartRadius,
+        borderBottomEndRadius: props.borderBottomEndRadius,
+        alignSelf: 'center',
+        elevation:4,
+      }}
+      buttonStyle={{
+        elevation:1,
+        shadowOffset: {
+          width: 0,
+          height: 1, // Reduza a altura da sombra
+        },
+        borderRadius:10,
+        minHeight:props.minHeightButton
+      }}
+    >
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Icon
+          name={props.leftIconName}
+          type='font-awesome'
+          size={15}
+          color='white'
+          style={{ marginHorizontal: 10 }}
+          onPress={props.onLeftIconPress}
+        />
+        <Text style={buttonStyle.buttonText}>{props.title}</Text>
+        <Icon
+          name={props.rightIconName}
+          type='font-awesome'
+          size={15}
+          color='white'
+          style={{ marginHorizontal: 10 }}
+          onPress={props.onRightIconPress}
+        />
+      </View>
+    </Button>
+  );
 };
 
 export default ButtonComponent;
